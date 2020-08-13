2bd7ffa32b0f3f157b078cb40e1db21e6b5ff43c
mrkeksz <mrkeksz@yandex.ru>
2020-08-13 15:10:43 +0300

    Initial dist subtree commit

---
+++ dist/precache-manifest.aaf600b96632ca2aad223aef998ee497.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388702344fe1916385c3d300212cccb8",
+    "url": "/medods-test-form/index.html"
+  },
+  {
+    "revision": "aa787ed566b0e3796d2ed66cf85698d6",
+    "url": "/medods-test-form/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/medods-test-form/robots.txt"
+  },
+  {
+    "revision": "e793c1210d0def5c0bd1",
+    "url": "/medods-test-form/static/css/app.985a2ad7.css"
+  },
+  {
+    "revision": "e793c1210d0def5c0bd1",
+    "url": "/medods-test-form/static/js/app.0a3ce8c6.js"
+  },
+  {
+    "revision": "be75e440b0518d02fbf3",
+    "url": "/medods-test-form/static/js/chunk-vendors.140457bf.js"
+  }
+]);
